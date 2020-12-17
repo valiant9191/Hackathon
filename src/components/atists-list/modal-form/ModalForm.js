14e166f5c7b17498d67a1a1e949592933238f323
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2020-12-17 16:55:20 +0000

    created ModalForm

---
+++ src/components/atists-list/modal-form/ModalForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ModalForm.css'
+
+const ModalForm = () => {
+
+
+    return (
+        
+        <div>
+
+        </div>
+
+    )
+}
+
+export default ModalForm;
