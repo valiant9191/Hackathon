c13cb6e78417ac24d0224fb3a8c24626d70e3b33
Kolko Valentin <valiant9191@gmail.com>
2020-12-17 23:08:13 +0300

    about us, add data team

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './AboutUs.css'
+import TeamCard from './team-member-card';
+import  dataForTeamCard  from './team-member-card/dataForTeamCard'
+
+
+const dataTeam = dataForTeamCard;
 
 const AboutUs = () => {
     return(
+    <div>
         <h1>Im About Us</h1>
+        {dataTeam.map((el)=>(<TeamCard  
+            title={el.name}
+        />))}
+    </div>
     )
 }
 
---
+++ src/components/about-us/team-member-card/dataForTeamCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+
+
+const dataForTeamCard = [
+    {inqusitor: 'Gabriellji',
+     photo:'src\components\about-us\team-member-card\srcphotos\PhotoFunia-Gabriell.jpg',
+     linkGithub: "https://github.com/Gabriellji",
+     description: '',
+    },
+    {inqusitor: 'Aya Berdyeva',
+    photo:'src\components\about-us\team-member-card\srcphotos\PhotoFunia-Aya.jpg',
+    linkGithub: "https://github.com/Gabriellji",
+    description: '',
+    },
+    {inqusitor: 'Viet Quoc Pham',
+    photo:'src\components\about-us\team-member-card\srcphotos\PhotoFunia-Viet.jpg',
+    linkGithub: "https://github.com/VietQuoc356",
+    description: '',
+    },
+
+    {inqusitor: 'Valentin Kolko',
+    photo:'src\components\about-us\team-member-card\srcphotos\PhotoFunia-1608229689.jpg',
+    linkGithub: "https://github.com/valiant9191",
+    description: '',
+    
+    },
+     {inqusitor: 'Dima',
+     photo:'',
+     linkGithub: "https://github.com/Gabriellji",
+     description: '',
+    }
+]
+export default dataForTeamCard;
