babc7e2af2481a4d6cbd17d4151f1dfb5955ca23
Gabriellji <gabriellji@yahoo.com>
2020-12-23 18:20:37 +0300

    fix: bugs

--- src/components/personal-info/PersonalPage.js
+++ src/components/personal-info/PersonalPage.js
@@ -25,18 +25,26 @@ const PersonalPage = () => {
         setIsOpen(!isOpen);
     }
 
-    //const [state, setState] = useState('');
+    const [state, setState] = useState('');
 
-    //const person = context.artists[0];
+    const person = context.artists[0];
 
-    // const getData = () => {
-    //         axios
-    //             .get(`https://openaccess-api.clevelandart.org/api/artworks/?q=${person.name}&skip=2&limit=1&indent=1`)
-    //             .then(data => {
-    //                 setState(data.data)
-    //                 console.log(state)
-    //             })
-    // }
+    // useEffect(() => (
+    //     getData()
+    // ), [])
+
+
+
+    const getData = () => {
+        axios
+            .get(`https://openaccess-api.clevelandart.org/api/artworks/?q=${person.name}&skip=2&limit=1&indent=1`)
+            .then(pizza => {
+                setState(pizza.data)
+                console.log(state)
+            })
+    }
+
+    // getData();
     const pics = [
         "https://images.theconversation.com/files/256856/original/file-20190201-75085-1mor4cs.png?ixlib=rb-1.1.0&rect=3%2C23%2C477%2C533&q=45&auto=format&w=926&fit=clip",
         "https://upload.wikimedia.org/wikipedia/commons/5/5e/Miguel_%C3%81ngel%2C_por_Daniele_da_Volterra_%28detalle%29.jpg",
@@ -50,27 +58,28 @@ const PersonalPage = () => {
         "https://upload.wikimedia.org/wikipedia/commons/2/22/Giorgio_Vasari_Selbstportr%C3%A4t.jpg",
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbg_93dbzND97lrjwnPzhp3HQZOe3RLBxBQ&usqp=CAU",
         "https://upload.wikimedia.org/wikipedia/commons/3/39/Cinque_maestri_del_rinascimento_fiorentino%2C_XVI_sec%2C_paolo_uccello.JPG",
-    
+
     ];
 
     const number = Math.floor(Math.random() * 10) + 1;
 
     return (
         <div className="peron-page_wrap">
+            {/* {context.artists && getData()} */}
             {
                 context.artists && <PersonInfo
-                artworks={pics[number]}
+                    artworks={pics[number]}
                     id={context.person.id}
                     name={context.person.name}
                     nationality={context.person.nationality}
                     description={context.person.description}
                     biography={context.person.biography}
                     birth_year={context.person.birth_year}
-                    
+
                 />
             }
             <div className="slider_wrap">
-            <h1>See examples of work</h1>
+                <h1>See examples of work</h1>
                 <Slider />
                 <h1>$$ 100 $$ dinar per hour</h1>
                 <ButtonWrap
